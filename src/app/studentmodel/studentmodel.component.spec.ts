5e89be2d8a4a07bb46b62a9f2432b0290b88fd4f
desoga <done2stay@gmail.com>
2019-05-14 19:40:03 +0100

    created json server and student compoet and service

---
+++ src/app/studentmodel/studentmodel.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StudentmodelComponent } from './studentmodel.component';
+
+describe('StudentmodelComponent', () => {
+  let component: StudentmodelComponent;
+  let fixture: ComponentFixture<StudentmodelComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ StudentmodelComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StudentmodelComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
